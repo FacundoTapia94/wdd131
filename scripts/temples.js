ba9a3e234a212b7f7281e46230e8221c67d89447
FacundoTapia94 <ftapia1@byupathway.edu>
2026-03-13 17:37:37 -0300

    Assigment week02

---
+++ scripts/temples.js
@@ -0,0 +1,13 @@
+
+const year = new Date().getFullYear();
+
+document.getElementById("currentyear").textContent = year;
+document.getElementById("lastModified").innerHTML = document.lastModified;
+
+const hamButton = document.querySelector("#menu")
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    hamButton.classList.toggle("open");
+});
